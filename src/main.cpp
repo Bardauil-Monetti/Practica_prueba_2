87669db19d168ffdfe4cc5705af66a727b6b460b
Bardauil-Monetti <gbardauil548@gmail.com>
2025-06-30 10:04:29 -0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
+//Punto 1; Consigna: Cambiar estado de LED mediante un pulsador como interrupcion.
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int bot = 1; //boton (interrupcion)
+int led = 2; //led
+volatile bool flag = false;
+bool estado = false;
+void IRAM_ATTR ISR() {
+  flag = !flag; 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(bot, INPUT_PULLUP); //Estado logico normal del boton --> 1
+  attachInterrupt(digitalPinToInterrupt(bot), ISR, FALLING);
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+  if(flag){
+    estado = !estado;
+    flag = !flag;
+  }
+  digitalWrite(led, estado);
 }
